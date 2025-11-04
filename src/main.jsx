96c08b9e795d689a13a824003a4be1b6e3632371
produtividadecaotica <produtividadecaotica@gmail.com>
2025-11-04 19:17:25 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
-// src/main.jsx
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-
-import "./index.css";
 import App from "./App.jsx";
-
-// garante que o splash suma quando o app montar
-try { window.__pc_ready__ && window.__pc_ready__(); } catch {}
 
 createRoot(document.getElementById("root")).render(
   <HashRouter>
