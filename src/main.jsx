f7c6522cac6b9f745f61ce269ccbc7f997dab8e7
produtividadecaotica <produtividadecaotica@gmail.com>
2025-11-04 17:56:45 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,31 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
-// Se você usa providers, mante-los aqui:
-import Toaster from "./ui/toaster.jsx";
-import { ThemeProvider } from "./contexts/ThemeContext.jsx";
-import { CalendarProvider } from "./contexts/CalendarContext.jsx";
-import { ObjectiveProvider } from "./contexts/ObjectiveContext.jsx";
-import { ProjectProvider } from "./contexts/ProjectContext.jsx";
-
-function RootProviders({ children }) {
-  return (
-    <ThemeProvider>
-      <CalendarProvider>
-        <ObjectiveProvider>
-          <ProjectProvider>{children}</ProjectProvider>
-        </ObjectiveProvider>
-      </CalendarProvider>
-    </ThemeProvider>
-  );
-}
-
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <RootProviders>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-    </BrowserRouter>
-  </RootProviders>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
